e5db80e4aa7bdf20477d5be26f6bb01fa0055f82
mahgoub <mahgoub.soliman@bbc.co.uk>
2024-09-26 18:24:11 +0100

    hashmap improvments

--- kernel/bio.c
+++ kernel/bio.c
@@ -13,7 +13,6 @@
 // * Only one process at a time can use a buffer,
 //     so do not keep them longer than necessary.
 
-
 #include "types.h"
 #include "param.h"
 #include "spinlock.h"
@@ -22,100 +21,97 @@
 #include "defs.h"
 #include "fs.h"
 #include "buf.h"
+#include "hashmap.h"  // Hypothetical hash map implementation for fast lookups
+
+#define DEFAULT_NBUF 128
 
 struct {
   struct spinlock lock;
   struct buf buf[NBUF];
-
-  // Linked list of all buffers, through prev/next.
-  // Sorted by how recently the buffer was used.
-  // head.next is most recent, head.prev is least.
   struct buf head;
+  struct hashmap *cache_map;  // Hash map for fast buffer lookups
+  int cache_size;
 } bcache;
 
-void
-binit(void)
-{
+// Initialize the buffer cache
+void binit(void) {
   struct buf *b;
 
+  // Initialize global lock for the cache
   initlock(&bcache.lock, "bcache");
 
-  // Create linked list of buffers
+  // Initialize the LRU linked list and hash map
   bcache.head.prev = &bcache.head;
   bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
+  bcache.cache_map = hashmap_create();
+  bcache.cache_size = DEFAULT_NBUF;
+
+  // Create linked list of buffers and add to hash map
+  for(b = bcache.buf; b < bcache.buf + NBUF; b++){
     b->next = bcache.head.next;
     b->prev = &bcache.head;
     initsleeplock(&b->lock, "buffer");
     bcache.head.next->prev = b;
     bcache.head.next = b;
+    hashmap_put(bcache.cache_map, b->dev, b->blockno, b);  // Add buffer to hash map
   }
 }
 
-// Look through buffer cache for block on device dev.
-// If not found, allocate a buffer.
-// In either case, return locked buffer.
-static struct buf*
-bget(uint dev, uint blockno)
-{
+// Get a cached buffer or allocate a new one
+static struct buf* bget(uint dev, uint blockno) {
   struct buf *b;
 
   acquire(&bcache.lock);
 
-  // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
-    if(b->dev == dev && b->blockno == blockno){
-      b->refcnt++;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
-    }
+  // Try to find the buffer in the cache
+  b = hashmap_get(bcache.cache_map, dev, blockno);
+  if (b != NULL) {
+    b->refcnt++;
+    release(&bcache.lock);
+    acquiresleep(&b->lock);
+    return b;
   }
 
-  // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
+  // Buffer not cached; recycle least recently used (LRU) buffer
   for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
     if(b->refcnt == 0) {
       b->dev = dev;
       b->blockno = blockno;
       b->valid = 0;
       b->refcnt = 1;
+      hashmap_put(bcache.cache_map, dev, blockno, b);  // Add new buffer to cache
       release(&bcache.lock);
       acquiresleep(&b->lock);
       return b;
     }
   }
+
+  // No available buffer; retry or put the process to sleep
+  release(&bcache.lock);
   panic("bget: no buffers");
 }
 
-// Return a locked buf with the contents of the indicated block.
-struct buf*
-bread(uint dev, uint blockno)
-{
+// Read the contents of a block into a buffer
+struct buf* bread(uint dev, uint blockno) {
   struct buf *b;
 
   b = bget(dev, blockno);
   if(!b->valid) {
-    virtio_disk_rw(b, 0);
+    virtio_disk_rw(b, 0);  // Read from disk
     b->valid = 1;
   }
   return b;
 }
 
-// Write b's contents to disk.  Must be locked.
-void
-bwrite(struct buf *b)
-{
+// Write buffer contents to disk
+void bwrite(struct buf *b) {
   if(!holdingsleep(&b->lock))
     panic("bwrite");
-  virtio_disk_rw(b, 1);
+  virtio_disk_rw(b, 1);  // Write to disk
 }
 
-// Release a locked buffer.
-// Move to the head of the most-recently-used list.
-void
-brelse(struct buf *b)
-{
+// Release a buffer and move it to the head of the LRU list
+void brelse(struct buf *b) {
   if(!holdingsleep(&b->lock))
     panic("brelse");
 
@@ -124,7 +120,7 @@ brelse(struct buf *b)
   acquire(&bcache.lock);
   b->refcnt--;
   if (b->refcnt == 0) {
-    // no one is waiting for it.
+    // No one is using it, move to head of the LRU list
     b->next->prev = b->prev;
     b->prev->next = b->next;
     b->next = bcache.head.next;
@@ -136,18 +132,27 @@ brelse(struct buf *b)
   release(&bcache.lock);
 }
 
-void
-bpin(struct buf *b) {
+// Pin the buffer in cache to prevent eviction
+void bpin(struct buf *b) {
   acquire(&bcache.lock);
   b->refcnt++;
   release(&bcache.lock);
 }
 
-void
-bunpin(struct buf *b) {
+// Unpin the buffer and allow eviction
+void bunpin(struct buf *b) {
   acquire(&bcache.lock);
+  if (b->refcnt <= 0) {
+    panic("bunpin: refcnt already zero");
+  }
   b->refcnt--;
   release(&bcache.lock);
 }
 
+// Optionally set cache size dynamically
+void set_cache_size(int size) {
+  acquire(&bcache.lock);
+  bcache.cache_size = size;
+  release(&bcache.lock);
+}
 
